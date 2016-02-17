bdfea858bd4d0674fd8dd702eafb6a0c2032d951
Richard Rollins <rprollins@users.noreply.github.com>
2016-02-17 11:28:21 +0000

    Consolidated repeated code, fixed P2 initialization

--- benchmarks/Benchmark_walls.cc
+++ benchmarks/Benchmark_walls.cc
@@ -5,10 +5,12 @@ class Walls
 
 public:
 
-  typedef Grid::iScalar<Grid::vReal> vScalar;
-  typedef Grid::Lattice<vScalar>     vScalarField;
+  typedef Grid::iScalar<Grid::vRealD> vScalar;
+  typedef Grid::Lattice<vScalar>      vScalarField;
+  typedef vScalarField *              pvScalarField;
+  typedef vScalarField const *        pcvScalarField;
 
-  Walls(Grid::GridCartesian * Grid) :
+  Walls(Grid::GridCartesian * Grid, vScalarField * P1, vScalarField * P2) :
 
     stencil(Grid,npoint,Even,directions,displacements),
     dct(tampasso * ctinitial),
@@ -22,43 +24,18 @@ public:
   {
     // Resize communication buffer
     comm_buf.resize(stencil._unified_buffer_size);
+
+    // Initialize pointers to scalar fields
+    Pold = P2;
+    Pnow = P1;
+    Pnew = P2;
+
+    // Initialize time varaibles
+    ct_prev = ctinitial;
+    ct      = ctinitial;
   }
 
-  void initialise(Walls::vScalarField * Pnew, const Walls::vScalarField * Pnow, const Walls::vScalarField * Pold)
-  {
-    // Initialise global varaibles
-    ct         = ctinitial;
-    delta      = 0.5 * alfa * expoentec * dct / ct;
-    V04atbeta0 = 4.0 * V0 * pow( pow(ctinitial,expoentec), beta0 );
-    m1delta    = 1.0 - delta;
-    i1pdelta   = 1.0 / (1.0 + delta);
-
-    // Halo exchange
-    stencil.HaloExchange(*Pnow,comm_buf,compressor);
-
-    // Iterate over all grid sites
-    PARALLEL_FOR_LOOP
-    for(int i=0; i<Pnow->_grid->oSites(); i++)
-    {
-      // Laplacian
-      vScalar Lphi = Laplacian(Pnow,stencil,comm_buf,i);
-      // Current time derivative
-      vScalar DPnow_ijkl = Grid::zero;
-      // New time derivative
-      vScalar DPnew_ijkl = (m1delta * DPnow_ijkl + dct * (Lphi - V04atbeta0 *((Pnow->_odata[i]*(Pnow->_odata[i]*Pnow->_odata[i]-1.0)) + qmiu))) * i1pdelta;
-      // Dynamical evolution of P
-      Pnew->_odata[i] = Pnow->_odata[i] + dct * DPnew_ijkl;
-    }
-
-    // Halo exchange
-    stencil.HaloExchange(*Pnew,comm_buf,compressor);
-
-    // Increment time
-    ct_prev = ct;
-    ct += dct;
-  }
-
-  void timestep(Walls::vScalarField * Pnew, const Walls::vScalarField * Pnow, const Walls::vScalarField * Pold)
+  void timestep()
   {
     // Calculate global varaibles
     delta      = 0.5 * alfa * expoentec * dct / ct;
@@ -71,7 +48,7 @@ public:
     for(int i=0; i<Pnow->_grid->oSites(); i++)
     {
       // Laplacian
-      vScalar Lphi = Laplacian(Pnow,stencil,comm_buf,i);
+      vScalar Lphi = Laplacian(i);
       // Current time derivative
       vScalar DPnow_ijkl = (Pnow->_odata[i] - Pold->_odata[i]) * idct;
       // New time derivative
@@ -84,11 +61,55 @@ public:
     stencil.HaloExchange(*Pnew,comm_buf,compressor);
 
     // Increment time
-    ct_prev = ct;
-    ct += dct;
+    ct_prev  = ct;
+    ct      += dct;
+
+    // Switch array aliases
+    Pold = const_cast<vScalarField const *>(Pnow);
+    Pnow = const_cast<vScalarField const *>(Pnew);
+    Pnew = const_cast<vScalarField *>(Pold);
   }
 
-  Walls::vScalar Laplacian(const Walls::vScalarField * Pnow, Grid::CartesianStencil & stencil, std::vector< Walls::vScalar, Grid::alignedAllocator<Walls::vScalar> > & comm_buf, int i)
+private:
+
+  const int ndim   = 3;
+  const int npoint = 6;
+  const int Even   = 0;
+  const std::vector<int> directions    = std::vector<int>({ 0, 1, 2, 0, 1, 2});
+  const std::vector<int> displacements = std::vector<int>({ 1, 1, 1,-1,-1,-1});
+
+  Grid::CartesianStencil stencil;
+  std::vector< vScalar, Grid::alignedAllocator<vScalar> > comm_buf;
+  Grid::SimpleCompressor<vScalar> compressor;
+
+  const Grid::Real alfa       = 3.0;
+  const Grid::Real beta0      = 0.0;
+  const Grid::Real W0         = 10.0;
+
+  const Grid::Real expoente   = 0.6666666;
+  const Grid::Real ctinitial  = 1.0;
+  const Grid::Real tampasso   = 0.25;
+
+  const Grid::Real dct;
+  const Grid::Real expoentec;
+  const Grid::Real dx2r;
+  Grid::Real ct;
+  Grid::Real ct_prev;
+  const Grid::Real omega0;
+  const Grid::Real V0;
+
+  Grid::Real delta;
+  const Grid::Real idct;
+  Grid::Real m1delta;
+  Grid::Real V04atbeta0;
+  const Grid::Real qmiu;
+  Grid::Real i1pdelta;
+
+  vScalarField const * Pold;
+  vScalarField const * Pnow;
+  vScalarField *       Pnew;
+
+  vScalar Laplacian(int i)
   {
     // Variables used by stencil
     Grid::StencilEntry *SE;
@@ -118,41 +139,6 @@ public:
     Lphi -= 6.0 * Pnow->_odata[i];
     return Lphi * dx2r;
   }
-
-private:
-
-  const int ndim   = 3;
-  const int npoint = 6;
-  const int Even   = 0;
-  const std::vector<int> directions    = std::vector<int>({ 0, 1, 2, 0, 1, 2});
-  const std::vector<int> displacements = std::vector<int>({ 1, 1, 1,-1,-1,-1});
-
-  Grid::CartesianStencil stencil;
-  std::vector< Walls::vScalar, Grid::alignedAllocator<Walls::vScalar> > comm_buf;
-  Grid::SimpleCompressor<Walls::vScalar> compressor;
-
-  const Grid::Real alfa       = 3.0;
-  const Grid::Real beta0      = 0.0;
-  const Grid::Real W0         = 10.0;
-
-  const Grid::Real expoente   = 0.6666666;
-  const Grid::Real ctinitial  = 1.0;
-  const Grid::Real tampasso   = 0.25;
-
-  const Grid::Real dct;
-  const Grid::Real expoentec;
-  const Grid::Real dx2r;
-  Grid::Real ct;
-  Grid::Real ct_prev;
-  const Grid::Real omega0;
-  const Grid::Real V0;
-
-  Grid::Real delta;
-  const Grid::Real idct;
-  Grid::Real m1delta;
-  Grid::Real V04atbeta0;
-  const Grid::Real qmiu;
-  Grid::Real i1pdelta;
 };
 
 int main (int argc, char ** argv)
@@ -160,98 +146,53 @@ int main (int argc, char ** argv)
   // Initialize grid library
   Grid::Grid_init(&argc,&argv);
 
-  // Scalar field in Grid
-  typedef Grid::iScalar<Grid::vReal> vScalar;
-  typedef Grid::Lattice<vScalar>     vScalarField;
+  // Benchmark constants
+  const int Nloop = 100000; // Benchmark iterations
+  const int Nd    = 3;      // Three spatial dimensions
 
-  int Nloop = 100000; // Benchmark iterations
-  int Nd    = 3;      // Three spatial dimensions
-
-  // SIMD and MPI layouts
-  std::vector<int> simd_layout = Grid::GridDefaultSimd(Nd,Grid::vReal::Nsimd());
-  std::vector<int> mpi_layout  = std::vector<int>(Nd,1);
-
-  // OpenMP thread count
+  // Lattice, MPI and SIMD layouts and OpenMP threads
+  std::vector<int> latt_size   = Grid::GridDefaultLatt();
+  std::vector<int> mpi_layout  = Grid::GridDefaultMpi();
+  std::vector<int> simd_layout = Grid::GridDefaultSimd(Nd,Grid::vRealD::Nsimd());
   int threads = Grid::GridThread::GetThreads();
-  std::cout << Grid::GridLogMessage << "Grid is setup to use "<<threads<<" threads"<<std::endl;
+  int vol     = latt_size[0]*latt_size[1]*latt_size[2];
 
-  // Standard output header
-  std::cout << Grid::GridLogMessage << "====================================================================================================" <<std::endl;
-  std::cout << Grid::GridLogMessage << "= Benchmarking Walls 3D" << std::endl;
-  std::cout << Grid::GridLogMessage << "====================================================================================================" <<std::endl;
-  std::cout << Grid::GridLogMessage << "  L  " << "\t\t" << "bytes" << "\t\t\t" << "GB/s" << "\t\t" << "Gflop/s" << std::endl;
-  std::cout << Grid::GridLogMessage << "----------------------------------------------------------" << std::endl;
+  // Cartesian grid
+  Grid::GridCartesian Grid(latt_size,simd_layout,mpi_layout);
 
-  // Iterate over different local grid sizes given by lat.lat.lat
-  for(int lat=4; lat<=32; lat+=4)
-  {
-    // Lattice size and volume
-    std::vector<int> latt_size ({lat*mpi_layout[0],lat*mpi_layout[1],lat*mpi_layout[2]});
-    int vol = latt_size[0]*latt_size[1]*latt_size[2];
+  // Random number genereator
+  Grid::GridParallelRNG pRNG(&Grid);
+  pRNG.SeedRandomDevice();
 
-    // Cartesian grid
-    Grid::GridCartesian Grid(latt_size,simd_layout,mpi_layout);
+  // Scalar fields P1 and P2 are initialized to the same sample
+  // from the uniform random field [-1.0,1.0] on the Grid
+  Walls::vScalarField P1(&Grid);
+  Walls::vScalarField P2(&Grid);
+  random(pRNG,P1);
+  P1 = (2.*P1) - 1.;
+  P2 = P1;
 
-    // Random number genereator
-    Grid::GridParallelRNG pRNG(&Grid);
-    pRNG.SeedRandomDevice();
+  // Walls object
+  Walls walls(&Grid,&P1,&P2);
 
-    // Scalar fields
-    Walls::vScalarField P1(&Grid);
-    Walls::vScalarField P2(&Grid);
-    Walls::vScalarField * Pold;
-    Walls::vScalarField * Pnow;
-    Walls::vScalarField * Pnew;
+  // Start timer
+  double start = Grid::usecond();
 
-    // Initialise scalar fields
-    random(pRNG,P1);
-    P2 = Grid::zero;
-    Pold = NULL;
-    Pnow = &P1;
-    Pnew = &P2;
+  // Loop over timesteps
+  for(int ii=0; ii<Nloop; ii++) { walls.timestep(); }
 
-    // Check that P1 and P2 are consistent
-    Grid::conformable(P1._grid,P2._grid);
-    P2.checkerboard = P1.checkerboard;
+  // Total run time
+  double stop = Grid::usecond();
+  double time = (stop-start) * 1.0E-6;
 
-    // Walls object
-    Walls walls(&Grid);
+  // Memory throughput and Flop rate
+  double bytes = vol * sizeof(Grid::Real) * 2 * Nloop;
+  double flops = double(vol) * double(22) * double(Nloop);
+  std::cout << Grid::GridLogMessage << std::setprecision(5)
+            << "GB/s: "   << 1.0E-9 * bytes / time << "\t"
+            << "Flop/s: " << 1.0E-9 * flops / time << std::endl;
 
-    // Initial time step
-    walls.initialise(Pnew,Pnow,Pold);
-
-    // Switch array aliases
-    Pold = Pnow;
-    Pnow = Pnew;
-    Pnew = Pold;
-
-    // Start timer
-    double start = Grid::usecond();
-
-    // Loop over timesteps
-    for(int ii=0; ii<Nloop; ii++)
-    {
-      // Initial time step
-      walls.timestep(Pnew,Pnow,Pold);
-
-      // Switch array aliases
-      Pold = Pnow;
-      Pnow = Pnew;
-      Pnew = Pold;
-    }
-
-    // Total run time
-    double stop = Grid::usecond();
-    double time = (stop-start) * 1.0E-6;
-
-    // Memory throughput and Flop rate
-    double bytes = vol * sizeof(Grid::Real) * 2 * Nloop;
-    double flops = double(vol) * double(20) * double(Nloop);
-    std::cout << Grid::GridLogMessage << std::setprecision(3) << lat
-              << "\t\t" << bytes << " \t\t" << 1.0E-9 * bytes / time
-              << "\t\t" << 1.0E-9 * flops / time << std::endl;
-  }
-
+  // Finalize
   Grid::Grid_finalize();
   return(0);
 }
